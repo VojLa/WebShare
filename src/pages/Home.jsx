1a1146263f5421a6d08dd22a96095ef6dc0828f3
VojLa <lacinavojtech@outlook.com>
2026-04-16 18:56:31 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,12 +6,20 @@ import WhyUsSection from '../components/home/WhyUsSection';
 import EServisSection from '../components/home/EServisSection';
 import CTASection from '../components/home/CTASection';
 import { homeContent } from '../content/home';
+import useTranslation from '../hooks/useTranslation';
 
 export default function Home() {
+  const { t } = useTranslation();
+
   return (
     <div>
       <HeroSection content={homeContent.hero} />
-      <StatsSection stats={homeContent.stats} />
+      <StatsSection stats={[
+                            { value: homeContent.stats[0], label: t('stats.label1') },
+                            { value: homeContent.stats[1], label: t('stats.label2') },
+                            { value: homeContent.stats[2], label: t('stats.label3') },
+                            { value: homeContent.stats[3], label: t('stats.label4') },
+                          ]} />
       <ServicesSection />
       <KDPSection />
       <WhyUsSection />
