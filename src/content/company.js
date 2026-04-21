ffc12a87bec2ff13c75de15ebfb178ce1170d973
Vojtech Lacina <lacinavojtech@outlook.com>
2026-04-21 17:05:16 +0200

    Changing czech lanlague

--- src/content/company.js
+++ src/content/company.js
@@ -5,7 +5,7 @@ export const companyInfo = {
     email: 'office@bestaccount.cz',
   },
   registration: {
-    registeredOffice: 'Komenskeho 152, Vitkov, 749 01',
+    registeredOffice: 'Komenského 152, Vítkov, 749 01',
     bankAccount: '9338116001/5500',
     dataBox: 'thrvd6r',
     ico: '05067014',
@@ -16,14 +16,14 @@ export const companyInfo = {
   },
   offices: {
     headquarters: {
-      addressFull: 'Wolkerova 1009, 749 01 Vitkov',
-      addressShort: 'Wolkerova 1009, Vitkov',
+      addressFull: 'Wolkerova 1009, 749 01 Vítkov',
+      addressShort: 'Wolkerova 1009, Vítkov',
       phone: '+420 721 075 783',
       mapEmbedUrl: 'https://mapy.com/s/kebazarola',
     },
     branch: {
-      addressFull: 'U Posty 250/6, 746 01 Opava',
-      addressShort: 'U Posty 250/6, Opava',
+      addressFull: 'U Pošty 250/6, 746 01 Opava',
+      addressShort: 'U Pošty 250/6, Opava',
       phone: '+420 725 423 579',
       mapEmbedUrl: 'https://mapy.com/s/kogevunehe',
     },
